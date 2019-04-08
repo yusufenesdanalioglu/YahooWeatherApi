6d00ae5b5b935fc93e4fdb0981138556539ed5a7
yedanalioglu <41891933+yedanalioglu@users.noreply.github.com>
2019-04-08 22:43:45 +0300

    YahooWeatherApi
    YahooWeather Api'si ile hava durumu verilerinin çekilerek kullanıcıya gösterilmesini içeren C# projesi

---
+++ yahooweatherService/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace yahooweatherService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtbPazar = new System.Windows.Forms.RichTextBox();
+            this.rtbCumartesi = new System.Windows.Forms.RichTextBox();
+            this.rtbCuma = new System.Windows.Forms.RichTextBox();
+            this.rtbPersembe = new System.Windows.Forms.RichTextBox();
+            this.rtbCarsamba = new System.Windows.Forms.RichTextBox();
+            this.rtbSali = new System.Windows.Forms.RichTextBox();
+            this.rtbPazartesi = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbCityName = new System.Windows.Forms.RichTextBox();
+            this.rtbCountryName = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.rtbPazar);
+            this.groupBox1.Controls.Add(this.rtbCumartesi);
+            this.groupBox1.Controls.Add(this.rtbCuma);
+            this.groupBox1.Controls.Add(this.rtbPersembe);
+            this.groupBox1.Controls.Add(this.rtbCarsamba);
+            this.groupBox1.Controls.Add(this.rtbSali);
+            this.groupBox1.Controls.Add(this.rtbPazartesi);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.rtbCityName);
+            this.groupBox1.Controls.Add(this.rtbCountryName);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1021, 348);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hava Durumu Servis";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(903, 129);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 26);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Pazar";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(730, 129);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(129, 26);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Cumartesi";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(611, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 26);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Cuma";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(447, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 26);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Perşembe";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(305, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 26);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Çarşamba";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(191, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 26);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Salı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 26);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Pazartesi";
+            // 
+            // rtbPazar
+            // 
+            this.rtbPazar.Location = new System.Drawing.Point(870, 158);
+            this.rtbPazar.Name = "rtbPazar";
+            this.rtbPazar.ReadOnly = true;
+            this.rtbPazar.Size = new System.Drawing.Size(138, 184);
+            this.rtbPazar.TabIndex = 13;
+            this.rtbPazar.Text = "";
+            // 
+            // rtbCumartesi
+            // 
+            this.rtbCumartesi.Location = new System.Drawing.Point(726, 158);
+            this.rtbCumartesi.Name = "rtbCumartesi";
+            this.rtbCumartesi.ReadOnly = true;
+            this.rtbCumartesi.Size = new System.Drawing.Size(138, 184);
+            this.rtbCumartesi.TabIndex = 12;
+            this.rtbCumartesi.Text = "";
+            // 
+            // rtbCuma
+            // 
+            this.rtbCuma.Location = new System.Drawing.Point(582, 158);
+            this.rtbCuma.Name = "rtbCuma";
+            this.rtbCuma.ReadOnly = true;
+            this.rtbCuma.Size = new System.Drawing.Size(138, 184);
+            this.rtbCuma.TabIndex = 9;
+            this.rtbCuma.Text = "";
+            // 
+            // rtbPersembe
+            // 
+            this.rtbPersembe.Location = new System.Drawing.Point(438, 158);
+            this.rtbPersembe.Name = "rtbPersembe";
+            this.rtbPersembe.ReadOnly = true;
+            this.rtbPersembe.Size = new System.Drawing.Size(138, 184);
+            this.rtbPersembe.TabIndex = 11;
+            this.rtbPersembe.Text = "";
+            // 
+            // rtbCarsamba
+            // 
+            this.rtbCarsamba.Location = new System.Drawing.Point(294, 158);
+            this.rtbCarsamba.Name = "rtbCarsamba";
+            this.rtbCarsamba.ReadOnly = true;
+            this.rtbCarsamba.Size = new System.Drawing.Size(138, 184);
+            this.rtbCarsamba.TabIndex = 10;
+            this.rtbCarsamba.Text = "";
+            // 
+            // rtbSali
+            // 
+            this.rtbSali.Location = new System.Drawing.Point(150, 158);
+            this.rtbSali.Name = "rtbSali";
+            this.rtbSali.ReadOnly = true;
+            this.rtbSali.Size = new System.Drawing.Size(138, 184);
+            this.rtbSali.TabIndex = 9;
+            this.rtbSali.Text = "";
+            // 
+            // rtbPazartesi
+            // 
+            this.rtbPazartesi.Location = new System.Drawing.Point(6, 158);
+            this.rtbPazartesi.Name = "rtbPazartesi";
+            this.rtbPazartesi.ReadOnly = true;
+            this.rtbPazartesi.Size = new System.Drawing.Size(138, 184);
+            this.rtbPazartesi.TabIndex = 8;
+            this.rtbPazartesi.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(36, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 26);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Şehir";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(36, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ülke";
+            // 
+            // rtbCityName
+            // 
+            this.rtbCityName.Location = new System.Drawing.Point(125, 75);
+            this.rtbCityName.Name = "rtbCityName";
+            this.rtbCityName.ReadOnly = true;
+            this.rtbCityName.Size = new System.Drawing.Size(298, 33);
+            this.rtbCityName.TabIndex = 1;
+            this.rtbCityName.Text = "";
+            // 
+            // rtbCountryName
+            // 
+            this.rtbCountryName.Location = new System.Drawing.Point(125, 36);
+            this.rtbCountryName.Name = "rtbCountryName";
+            this.rtbCountryName.ReadOnly = true;
+            this.rtbCountryName.Size = new System.Drawing.Size(298, 33);
+            this.rtbCountryName.TabIndex = 0;
+            this.rtbCountryName.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 348);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Yahoo Weather Api";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rtbCityName;
+        private System.Windows.Forms.RichTextBox rtbCountryName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtbPazar;
+        private System.Windows.Forms.RichTextBox rtbCumartesi;
+        private System.Windows.Forms.RichTextBox rtbCuma;
+        private System.Windows.Forms.RichTextBox rtbPersembe;
+        private System.Windows.Forms.RichTextBox rtbCarsamba;
+        private System.Windows.Forms.RichTextBox rtbSali;
+        private System.Windows.Forms.RichTextBox rtbPazartesi;
+    }
+}
+
---
+++ yahooweatherService/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections;
+
+namespace yahooweatherService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var client = new RestClient("https://weather-ydn-yql.media.yahoo.com/forecastrss?location=istanbul,tr&u=c&format=json&oauth_consumer_key=dj0yJmk9bDNNRGFzTGlUYWJWJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWU4&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1554719794&oauth_nonce=ue4u3PPgJNa&oauth_version=1.0&oauth_signature=C55VQk1vnxqhtcP8qAh6ZXD4ECM=");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Postman-Token", "92413044-2a9a-40a9-ae54-3e17fbd528ec");
+            request.AddHeader("cache-control", "no-cache");
+            IRestResponse response = client.Execute(request);
+
+
+            //Web Api'den dönen değerimizi Json formatında bir nesneye çevirdik ve bunu json tipindeki j nesnemize atama yaptık
+            JToken j = JToken.Parse(response.Content);
+            //şehir ismi verisi
+            rtbCityName.Text = j["location"]["city"].ToString();
+            //ülke ismi verisi
+            rtbCountryName.Text = j["location"]["country"].ToString();
+
+            int dayCount = 0;
+            //pazartesi verileri
+            rtbPazartesi.Text = rtbPazartesi.Text+" En Düşük Sıcaklık : " + j["forecasts"][dayCount]["low"].ToString()+" \n\r";
+            rtbPazartesi.Text = rtbPazartesi.Text + " En Yüksek Sıcaklık : " + j["forecasts"][dayCount]["high"].ToString() + " \n\r";
+            rtbPazartesi.Text = rtbPazartesi.Text + " Durum : " + j["forecasts"][dayCount]["text"].ToString() + " \n\r";
+
+            dayCount = 1;
+            //salı verileri
+            rtbSali.Text = rtbSali.Text + " En Düşük Sıcaklık : " + j["forecasts"][dayCount]["low"].ToString() + " \n\r";
+            rtbSali.Text = rtbSali.Text + " En Yüksek Sıcaklık : " + j["forecasts"][dayCount]["high"].ToString() + " \n\r";
+            rtbSali.Text = rtbSali.Text + " Durum : " + j["forecasts"][dayCount]["text"].ToString() + " \n\r";
+
+            dayCount = 2;
+            //çarşamba verileri
+            rtbCarsamba.Text = rtbCarsamba.Text + " En Düşük Sıcaklık : " + j["forecasts"][dayCount]["low"].ToString() + " \n\r";
+            rtbCarsamba.Text = rtbCarsamba.Text + " En Yüksek Sıcaklık : " + j["forecasts"][dayCount]["high"].ToString() + " \n\r";
+            rtbCarsamba.Text = rtbCarsamba.Text + " Durum : " + j["forecasts"][dayCount]["text"].ToString() + " \n\r";
+
+            dayCount = 3;
+            //perşembe verileri
+            rtbPersembe.Text = rtbPersembe.Text + " En Düşük Sıcaklık : " + j["forecasts"][dayCount]["low"].ToString() + " \n\r";
+            rtbPersembe.Text = rtbPersembe.Text + " En Yüksek Sıcaklık : " + j["forecasts"][dayCount]["high"].ToString() + " \n\r";
+            rtbPersembe.Text = rtbPersembe.Text + " Durum : " + j["forecasts"][dayCount]["text"].ToString() + " \n\r";
+
+            dayCount = 4;
+            //cuma verileri
+            rtbCuma.Text = rtbCuma.Text + " En Düşük Sıcaklık : " + j["forecasts"][dayCount]["low"].ToString() + " \n\r";
+            rtbCuma.Text = rtbCuma.Text + " En Yüksek Sıcaklık : " + j["forecasts"][dayCount]["high"].ToString() + " \n\r";
+            rtbCuma.Text = rtbCuma.Text + " Durum : " + j["forecasts"][dayCount]["text"].ToString() + " \n\r";
+
+            dayCount = 5;
+            //cumartesi verileri
+            rtbCumartesi.Text = rtbCumartesi.Text + " En Düşük Sıcaklık : " + j["forecasts"][dayCount]["low"].ToString() + " \n\r";
+            rtbCumartesi.Text = rtbCumartesi.Text + " En Yüksek Sıcaklık : " + j["forecasts"][dayCount]["high"].ToString() + " \n\r";
+            rtbCumartesi.Text = rtbCumartesi.Text + " Durum : " + j["forecasts"][dayCount]["text"].ToString() + " \n\r";
+
+            dayCount = 6;
+            //pazar verileri
+            rtbPazar.Text = rtbPazar.Text + " En Düşük Sıcaklık : " + j["forecasts"][dayCount]["low"].ToString() + " \n\r";
+            rtbPazar.Text = rtbPazar.Text + " En Yüksek Sıcaklık : " + j["forecasts"][dayCount]["high"].ToString() + " \n\r";
+            rtbPazar.Text = rtbPazar.Text + " Durum : " + j["forecasts"][dayCount]["text"].ToString() + " \n\r";
+
+            //Alttaki kod dönen tüm değerlerin tutulduğu bir nesne oluşturur
+            var tumData = JsonConvert.SerializeObject(response.Content.ToString());
+        }
+    }
+}
